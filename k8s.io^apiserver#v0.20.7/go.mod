f445c482b013fcecf66e39dae5736eae621b5050
hjc <2949815104@qq.com>
2022-12-04 15:57:31 +0800

    1

---
+++ k8s.io^apiserver#v0.20.7/go.mod
@@ -0,0 +1,5 @@
+module k8s.io/apiserver-v0.20.7
+
+go 1.17
+
+require k8s.io/apiserver v0.20.7
---
+++ k8s.io^apiserver#v0.20.9/main.go
@@ -0,0 +1,8 @@
+package k8s.io^apiserver#v0.20.9
+
+import _ "k8s.io/apiserver"
+import "fmt"
+
+func main() {
+    fmt.Println('a')
+}
